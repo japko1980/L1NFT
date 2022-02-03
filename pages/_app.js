6b7e3744cc00323f73c71c420121a8ee1d87031b
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-02-03 14:44:55 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function Marketplace({ Component, pageProps }) {
   return (
     <div>
       <nav className="border-b p-6">
-        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <p className="text-4xl font-bold">NFT on GenesisL1</p>
         <div className="flex mt-4">
           <Link href="/">
             <a className="mr-4 text-pink-500">
@@ -14,12 +14,12 @@ function Marketplace({ Component, pageProps }) {
           </Link>
           <Link href="/create-item">
             <a className="mr-6 text-pink-500">
-              Sell Digital Asset
+              Create NFT
             </a>
           </Link>
           <Link href="/my-assets">
             <a className="mr-6 text-pink-500">
-              My Digital Assets
+              My NFTs
             </a>
           </Link>
           <Link href="/creator-dashboard">
