000c7926d5eb20120ca4e3daed1a0adaaf2be2aa
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-02-03 15:00:42 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function Marketplace({ Component, pageProps }) {
   return (
     <div>
       <nav className="border-b p-6">
-        <p className="text-4xl font-bold">NFT on GenesisL1</p>
+        <p className="text-4xl font-bold">L1 NFT</p>
         <div className="flex mt-4">
           <Link href="/">
             <a className="mr-4 text-pink-500">
