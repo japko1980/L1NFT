7259a61928ddd00f260a883625681bcd6d3050b0
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-02-04 00:03:42 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function Marketplace({ Component, pageProps }) {
   return (
     <div>
       <nav className="border-b p-6">
-        <p className="text-4xl font-bold">L1 NFT</p>
+        <p className="text-4xl font-bold">L1NFT market</p>
         <div className="flex mt-4">
           <Link href="/">
             <a className="mr-4 text-pink-500">
@@ -14,7 +14,7 @@ function Marketplace({ Component, pageProps }) {
           </Link>
           <Link href="/create-item">
             <a className="mr-6 text-pink-500">
-              Create NFT
+              Create and sell NFT
             </a>
           </Link>
           <Link href="/my-assets">
