66d4bd550652193b9af6b8a7990f26aa4f7722a3
Fomolabs Dev <dev@fomolabs.io>
2022-02-17 20:42:32 +0100

    Add VR tools

--- pages/nft/[tokenId]/vrtools.js
+++ pages/nft/[tokenId]/vrtools.js
@@ -3,9 +3,13 @@ import { useEffect, useState, useRef } from 'react'
 import { ethers } from 'ethers'
 
 import axios from 'axios'
+import Web3Modal from "web3modal"
+
+import * as THREE from 'three';
 
 import { useRouter } from 'next/router'
 
+import { Item } from '../../../components/Item'
 
 import {
   nftaddress, nftmarketaddress
@@ -15,7 +19,7 @@ import NFT from '../../../artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../../../artifacts/contracts/Market.sol/NFTMarket.json'
 import { OneMinusDstAlphaFactor } from 'three'
 
-export default function VRTools() {
+export default function NFTView() {
 
   const router = useRouter();
   const el = useRef();
@@ -24,50 +28,34 @@ export default function VRTools() {
 
   const [nft, setNft] = useState()
   const [loadingState, setLoadingState] = useState('not-loaded')
+
+  const [ file, setFile ] = useState();
+  const [ size, setSize ] = useState();
+
+  useEffect(() => {
+    setSize([el.current.offsetWidth, el.current.offsetHeight])
+  }, [])
   
   useEffect(() => {
-
     tokenId && (async () => {
       const provider = new ethers.providers.JsonRpcProvider("https://rpcb.genesisL1.org")
       const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
       const tokenUri = await tokenContract.tokenURI(tokenId)
       const file = (await axios.get(tokenUri)).data
-      
-      const { data } = await axios.get(file.image);
-      //const ext = data.startsWith('HEADER') ? 'pdb' : 'cif';
-
-
+      setFile(file.image);
     })()
 
   }, [ tokenId ])
-
-  async function loadNFT() {
-    const provider = new ethers.providers.JsonRpcProvider("https://rpcb.genesisL1.org")
-    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
-    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
-    const data = await marketContract.fetchMarketItems()
-
-    const found = data.find(item => item.itemId.toNumber() === parseInt(tokenId))
-
-    found && setNft({
-      ...found,
-      price: ethers.utils.formatUnits(found.price.toString(), 'ether'),
-      getData: async () => {
-        const tokenUri = await tokenContract.tokenURI(tokenId)
-        try {
-          return (await axios.get(tokenUri)).data
-        } catch(error) {
-          return {}
-        }
-      },
-    });
-
-    setLoadingState('loaded')
-  }
   
   return (
-    <div className="flex justify-center">
-      <div className="px-4" ref={el} id="icn3dui" />
+    <div className="flex justify-center" style={{ flex: 1, display: 'flex' }} ref={el}>
+      {file && size &&
+      <iframe
+        allowFullScreen={true}
+        src={`https://vrmol.net/index.html?id=${file}&panelShow=1&mainMode=13&showSurface=1&surfaceOpc=1&surfaceType=1&colorMode=609&vmode=vr`}
+        style={{ border: 'none', flex: 1 }}
+      />
+      }
     </div>
   )
 }
