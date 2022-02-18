6b0a5f55acb090602c5dad0481027c1b6fdb09c7
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-02-17 22:18:02 -0500

    Update Item.js

--- components/Item.js
+++ components/Item.js
@@ -1,4 +1,3 @@
-import { useEffect, useState, useMemo } from 'react'
 import { useInView } from 'react-intersection-observer'
 
 import { ContentBox } from './ContentBox'
@@ -57,14 +56,14 @@ export const Item = ({
   <div className="p-4">
     <p style={{ height: '64px' }} className="text-2xl font-semibold">{data?.name}</p>
     <div style={{ height: '70px', overflow: 'hidden' }}>
-      <p className="text-gray-400">{data?.description}</p>
+      <p className="text-white lowercase text-justify">{data?.description}</p>
     </div>
   </div>
   <div className="p-4 bg-black">
     <p className="text-2xl mb-4 font-bold text-white">{data?.price} L1</p>
     
     <Actions>
-      <button className="bg-green-500 text-white font-bold py-2 px-4 rounded" onClick={() => buyNft(data)}>Buy</button>
+      <button className="bg-green-500 text-white font-bold py-2 px-4 rounded" onClick={() => buyNft(data)}>Accio</button>
       {onClick && <button className="bg-green-500 text-white font-bold py-2 px-4 rounded" onClick={onClick}>View</button>}
       <button className="bg-green-500 text-white font-bold py-2 px-4 rounded" onClick={onTools}>Analysis</button>
       <button className="bg-green-500 text-white font-bold py-2 px-4 rounded" onClick={onVR}>Bioverse</button>
