260620ed8b7004bd7fafe80db15c709e3504f9e9
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-02-20 15:01:55 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,30 +8,30 @@ function Marketplace({ Component, pageProps }) {
   return (
     <div className="bg-black">
       <nav className="text-center">
-        <h1 className="text-center text-white pt-6">MOLNFT</h1>
+        <h1 className="text-center text-white pt-6 text-3xl">MOLNFT</h1>
         <div className="mt-4">
           <Link href="/">
-            <a className="mr-4 text-white">
+            <a className="mr-4 text-white text-xl">
               Home
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-white">
+            <a className="mr-6 text-white text-xl">
               Create
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-white">
+            <a className="mr-6 text-white text-xl">
               My
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-white">
+            <a className="mr-6 text-white text-xl">
               Minted
             </a>
           </Link>
           <Link href="/about">
-            <a className="mr-6 text-white">
+            <a className="mr-6 text-white text-xl">
               About
             </a>
           </Link>
