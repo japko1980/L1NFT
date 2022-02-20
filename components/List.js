fc0986ff2068de2f97194f9d1f133eac710bc104
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-02-20 14:57:49 -0500

    4 to 3 items in row

--- components/List.js
+++ components/List.js
@@ -35,7 +35,7 @@ export const List = ({
   return(
   <div className="flex justify-center">
     <div className="px-4" style={{ maxWidth: '1600px' }}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-9 pt-4">
         {
           items.map((nft, i) =>
           <Item
